22709d953560d9c100a11df361c40c5b9c80eede
taesan <kts030701@gmail.com>
2026-04-19 01:24:54 +0900

    s

--- ox.js
+++ ox.js
@@ -1,6 +1,6 @@
 function ox(answer,link) {
 var userInput = document.getElementById("try").value;
-if (userInput.toLowerCase() === answer.toLowerCase())
+if (userInput.toLowerCase().replaceAll(' ', '') === answer.toLowerCase().replaceAll(' ', ''))
 { window.location.href = link;}
 else {alert("정답이 아닙니다.");}
 }
