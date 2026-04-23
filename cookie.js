13d102a7b2763f02d91aabb0d62ec68e05873b40
taesan <kts030701@gmail.com>
2026-04-23 20:27:40 +0900

    ㅁㄴㅇ

--- cookie.js
+++ cookie.js
@@ -1,5 +1,5 @@
 function setVisitCookie(x) {
-    document.cookie = "visit=" + x + "; path=/weblaby/; expires=Thu, 31 Dec 2026 23:59:59 GMT;"
+    document.cookie = "visit=" + x + "; path=/; max-age=31536000; Secure; SameSite=Lax";
 }
 function getCookie(name) {
   let cookies = document.cookie.split(";");
